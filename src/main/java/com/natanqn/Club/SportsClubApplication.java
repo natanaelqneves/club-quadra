dd5df881ff84cdbe1fd2e08b095732f7d42ba1bf
natanaelqn <natanaelqueiroz@gmail.com>
2024-06-07 23:37:10 -0300

    Arquivos iniciais base da aplicação Club

---
+++ src/main/java/com/natanqn/Club/SportsClubApplication.java
@@ -0,0 +1,13 @@
+package com.natanqn.Club;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsClubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsClubApplication.class, args);
+	}
+
+}
